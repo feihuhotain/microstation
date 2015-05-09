afe2bf0067f2f31ffc8facd0eb451e55b74a549b
feihuhotain <gperkin12125@gmail.com>
2015-05-09 13:11:22 -0700

    New

---
+++ objects/temp.cmd
@@ -0,0 +1,2 @@
+-oC:\mdl\mydialog\\objects\mydialog.ma
+C:\mdl\mydialog\\objects\mydialog.rsc  C:\mdl\mydialog\\objects\mydiatt.rsc  C:\mdl\mydialog\\objects\mydialog.mp
---
+++ objects/temp1.cmd
@@ -0,0 +1,4 @@
+-aC:\mdl\mydialog\\objects\mydialog.mp
+-s6000
+C:\mdl\mydialog\\objects\mydialog.mo
+C:\WIN32APP\USTATION\mdl\library\ditemlib.ml
