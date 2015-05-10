f34854203fc93fdc56c32cccbe7dffcfed99c1ee
feihuhotain <gperkin12125@gmail.com>
2015-05-09 18:23:50 -0700

    Test1

--- objects/temp.cmd
+++
@@ -1,2 +0,0 @@
--oC:\mdl\mydialog\\objects\mydialog.ma
-C:\mdl\mydialog\\objects\mydialog.rsc  C:\mdl\mydialog\\objects\mydiatt.rsc  C:\mdl\mydialog\\objects\mydialog.mp
--- objects/temp1.cmd
+++
@@ -1,4 +0,0 @@
--aC:\mdl\mydialog\\objects\mydialog.mp
--s6000
-C:\mdl\mydialog\\objects\mydialog.mo
-C:\WIN32APP\USTATION\mdl\library\ditemlib.ml
